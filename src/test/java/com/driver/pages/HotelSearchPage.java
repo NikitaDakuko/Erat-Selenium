10bf21e176f196693047c75232a92ebdb0b73e9c
Никита <Nikita.dakuko@gmail.com>
2020-01-04 04:24:24 +0300

    Added Successful login test

--- src/test/java/com/driver/pages/HotelSearchPage.java
+++ src/test/java/com/driver/pages/HotelSearchPage.java
@@ -1,13 +1,15 @@
 package com.driver.pages;
 
+import com.driver.utils.ResourceReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-import java.util.Properties;
+import java.util.List;
 
-public class HotelSearchPage extends SearchResultsPage {
+public class HotelSearchPage extends AbstractPage {
 
     @FindBy (id = "ss")
     private WebElement destination;
@@ -21,24 +23,38 @@ public class HotelSearchPage extends SearchResultsPage {
     @FindBy (className = "bui-calendar__control--next")
     private WebElement calendarNextButton;
 
-    protected HotelSearchPage(WebDriver driver) {
+    @FindBy(id = "hotellist_inner")
+    private WebElement hotelList;
+
+    private List<WebElement> results;
+
+    public HotelSearchPage(WebDriver driver){
         super(driver);
+        PageFactory.initElements(this.driver, this);
     }
 
+    @Override
+    public HotelSearchPage openPage() {
+        this.driver.get(ResourceReader.get("URL.Test.Search"));
+        return this;
+    }
 
-    public void setDestination(String string){
+    public HotelSearchPage setDestination(String string){
         System.out.println("Setting Destination");
         destination.sendKeys(string);
+        return this;
     }
 
-    public void setCalendar(String date1, String date2){
+    public HotelSearchPage setCalendar(String date1, String date2){
         System.out.println("Setting Date");
         calendar.click();
         calendar.findElement(By.xpath("//*[@data-date='"+ date1 + "']")).click();
         calendar.findElement(By.xpath("//*[@data-date='"+ date2 + "']")).click();
+        return this;
     }
 
-    public void checkPrices(){
+    public List<WebElement> checkPrices(){
         button.click();
+        return hotelList.findElements(By.className("sr_item_new"));
     }
 }
---
+++ src/test/java/com/driver/testing/SearchDataTests.java
@@ -0,0 +1,32 @@
+package com.driver.testing;
+
+import com.driver.pages.HotelSearchPage;
+import com.driver.utils.ResourceReader;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class SearchDataTests extends CommonConditions {
+
+    @Test
+    public void TestDestination() {
+        List<WebElement> result = new HotelSearchPage(driver)
+                .openPage()
+                .setDestination(ResourceReader.get("Search.Destination"))
+                .checkPrices();
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void TestDate() {
+        List<WebElement> result = new HotelSearchPage(driver)
+                .openPage()
+                .setDestination(ResourceReader.get("Search.Destination"))
+                .setCalendar(ResourceReader.get("Search.CheckIn.Date"),
+                             ResourceReader.get("Search.CheckOut.Date"))
+                .checkPrices();
+        Assert.assertNotNull(result);
+    }
+}
